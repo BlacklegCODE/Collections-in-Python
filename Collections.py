32631bf789c00fc2a19212aa129d1b998abeadaa
Harsh Raundal <dkingston0000@gmail.com>
2024-11-24 21:28:54 +0530

    Create Collections.py

---
+++ Collections.py
@@ -0,0 +1,33 @@
+
+# This code is practice of operations that can be done on a list
+
+
+#colledtion = single variable used to store multiple values
+
+fruit = ["Apples","Orange","Banana"]
+
+#print(fruit[:2])
+
+'''
+print(len(fruit))
+fruit.append("Apples")
+fruit.remove("Orange")
+fruit.insert(0,"Pineapple")
+fruit.sort()
+fruit.reverse()
+fruit.clear()
+print(fruit.index("Apple"))
+print(fruit.count("Banana"))'''
+
+
+#Below part is about operations done on a set, i.e. to add or remove elements
+
+#Set - unordered and immutable, but Add/Remove OK. No duplicates are allowed in it
+'''print(dir(fruit))
+fruit.pop()
+fruit.add("Pineapple")
+fruit.remove("Apple")
+fruit.clear()'''
+
+#tuples - ordered and unchangeable. Duplicate are allowed. These are faster
+
